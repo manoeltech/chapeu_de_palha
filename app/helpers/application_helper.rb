567de102678f9de79b9ee1cf97c6267652020aed
manoeltech <manoel.programador21@gmail.com>
2023-11-28 10:23:33 -0300

    include helper date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def formatter_date(created_at)
+    created_at.strftime("%d/%m/%Y")
+  end
+
+  def formatter_time(created_at)
+    created_at.strftime("%H:%M")
+  end
+
+  def formatter_money(value)
+    number_to_currency(value, unit: "R$", separator: ",", delimiter: ".")
+  end
 end
